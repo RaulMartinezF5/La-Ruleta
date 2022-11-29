2457caf217e5fefbbc25366a105f6f3a1fc151d6
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-11-29 19:02:52 +0100

    FInal de jornada

--- src/js/add-participants.js
+++ src/js/add-participants.js
@@ -52,7 +52,7 @@ function showParticipants() {
         parraph.dataset.id = id;
 
         const editButton = document.createElement("button");
-        editButton.onclick = () => editParticipants(id);
+        editButton.onclick = () => editParticipants(id, name);
         editButton.setAttribute('name',id);
         editButton.textContent = "Edit";
         parraph.append(editButton);
@@ -80,7 +80,7 @@ function showParticipants() {
         parraph.append(updateInput);
 
         const updateButton = document.createElement("button");
-        updateButton.onclick = () => updateParticipants(id,name);
+        updateButton.onclick = () => updateParticipants(id);
         updateButton.classList.add('invisible');
         updateButton.textContent = "Update";
         updateButton.setAttribute("name",name);
@@ -129,18 +129,25 @@ function deleteParticipants(id) {
     showParticipants();
 }
 
-function updateParticipants() {
-    objParticipant.name = nameInput.value;
+function updateParticipants(id) {
+    const nameUpdate = document.getElementById(id);
+    console.log(nameUpdate);
 
+    objParticipant.name = nameUpdate.value;
+    console.log(objParticipant.name);
     listParticipants.map(participants => {
         if (participants.id === objParticipant.id) {
             participants.id = objParticipant.id;
             participants.name = objParticipant.name;
             participants.desactivate = objParticipant.desactivate;
         }
-    });
 
+    });
     cleanObjetParticipant();
+    cancelParticipants();
+    cleanHtml();
+    showParticipants();
+
 }
 
 
@@ -155,8 +162,6 @@ function editParticipants(id, name) {
         allBtnSecondary.forEach(element => {
             element.style.display = 'inline';
         });
-console.log(allBtnPrimary)
-console.log(allBtnSecondary)      
     
 }
 
@@ -171,8 +176,5 @@ function cancelParticipants(id, name) {
     allBtnSecondary.forEach(element => {
         element.style.display = 'none';
     });
-    console.log(allBtnPrimary)
-    console.log(allBtnSecondary)      
-      
 
 }
